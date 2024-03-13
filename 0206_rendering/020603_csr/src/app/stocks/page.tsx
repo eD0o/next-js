c7e8f4970491d6d69430fd4bbd4e9fd267b19e07
Eduardo <eduardo.benigno15@gmail.com>
2024-03-13 19:39:45 -0300

    fix: identation stockspage 020603

--- 0206_rendering/020603_csr/src/app/stocks/page.tsx
+++ 0206_rendering/020603_csr/src/app/stocks/page.tsx
@@ -1,13 +1,22 @@
-export default async function StocksPage() {
-  const response = await fetch('https://api.origamid.online/acoes/lua', {
-    next: {
-      revalidate: 5,
-    },
-  });
-  const stock = (await response.json()) as {
-    simbolo: string;
-    atualizada: string;
-  };
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type Stock = {
+  simbolo: string;
+  atualizada: string;
+};
+
+export default function StocksPage() {
+  const [stock, setStock] = useState<Stock | null>(null);
+
+  useEffect(() => {
+    fetch('https://api.origamid.online/acoes/lua')
+      .then((response) => response.json())
+      .then((json) => setStock(json));
+  }, []);
+
+  if (stock === null) return null;
 
   return (
     <main>
