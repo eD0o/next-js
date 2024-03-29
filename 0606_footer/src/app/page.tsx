a76cd203b508939a7a79dac280f911bcc2018388
Eduardo <eduardo.benigno15@gmail.com>
2024-03-29 11:47:14 -0300

    feat: finishing 0605 and starting 0606

---
+++ 0606_footer/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="title">Dogs - Next.js</h1>
+    </main>
+  );
+}
---
+++ 0606_footer/src/components/header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import styles from './header.module.scss';
+import Image from 'next/image';
+
+export default async function Header() {
+  const user = true;
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} href={'/'}>
+          <Image
+            src={'/assets/dogs.svg'}
+            alt="Dogs"
+            width={28}
+            height={22}
+            priority
+          />
+        </Link>
+        {user ? (
+          <Link className={styles.login} href={'/account'}>Profile</Link>
+        ) : (
+          <Link className={styles.login} href={'/login'}>
+            Login
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+}
