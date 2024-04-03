bea4189315bcd6cb2b663faad634431b98b7158e
Eduardo <eduardo.benigno15@gmail.com>
2024-04-03 10:57:51 -0300

    feat: finishing 0614 preparing 0615

---
+++ 0615_login-reset/src/app/login/reset/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Reset your password.',
+  description: 'Reset your password.'
+}
+
+
+export default async function ResetPage() {
+  return (
+    <main>
+      <h1>Reset</h1>
+    </main>
+  );
+}
