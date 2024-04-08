d7b7776a32e2bc59eadf2d838bf772920e6fa23a
Eduardo <eduardo.benigno15@gmail.com>
2024-04-08 18:18:23 -0300

    feat: finishing 0622 and preparing 0623

---
+++ 0623_infinite-scroll/src/app/account/stats/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Stats'
+}
+
+export default async function StatsPage() {
+  return (
+    <main>
+      <h1>Stats</h1>
+    </main>
+  );
+}
