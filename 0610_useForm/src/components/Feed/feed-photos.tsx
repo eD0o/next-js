a197a0db9e4eb8a1ecdc3297e913056717187766
Eduardo <eduardo.benigno15@gmail.com>
2024-04-09 12:45:17 -0300

    fix: foto to photo url api

--- 0610_useForm/src/components/Feed/feed-photos.tsx
+++ 0610_useForm/src/components/Feed/feed-photos.tsx
@@ -8,7 +8,7 @@ export default function FeedPhotos({ photos }: { photos: Photo[] }) {
     <ul className={`${styles.feed} animeLeft`}>
       {photos.map((photo, i) => (
         <li className={styles.photo} key={photo.id + i}>
-          <Link href={`/foto/${photo.id}`} scroll={false}>
+          <Link href={`/photo/${photo.id}`} scroll={false}>
             <Image
               src={photo.src}
               width={1500}
