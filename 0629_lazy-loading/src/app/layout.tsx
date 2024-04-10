7f2f933ead89048d5bd61366528fd1faf9a0b2dc
Eduardo <eduardo.benigno15@gmail.com>
2024-04-10 20:49:08 -0300

    feat: finishing class 0628 and preparing 0629

---
+++ 0629_lazy-loading/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import './globals.scss';
+import { type_second } from '@/functions/fonts';
+import Header from '@/components/Header/header';
+import Footer from '@/components/Footer/footer';
+import { UserContextProvider } from '@/context/user-context';
+import userGet from '@/actions/user-get';
+
+export const metadata: Metadata = {
+  title: 'Dogs - Next.js',
+  description: 'Social Media for Dogs',
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const {data: user} = await userGet()
+
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={type_second.variable}>
+        <div className="App">
+          <UserContextProvider user={user}>
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </UserContextProvider>
+        </div>
+      </body>
+    </html>
+  );
+}
