4e58612c6a1114468cdfad275a4ce6fa198cd5fe
Eduardo <eduardo.benigno15@gmail.com>
2024-04-11 12:55:44 -0300

    feat: finishing 0632 and preparing 0633

---
+++ 0633_profile/src/actions/photos-get.ts
@@ -0,0 +1,46 @@
+'use server';
+
+import { PHOTOS_GET } from '@/functions/api';
+import apiError from '@/functions/api-error';
+
+export type Photo = {
+  id: number;
+  author: string;
+  title: string;
+  date: string;
+  src: string;
+  peso: string;
+  idade: string;
+  acessos: string;
+  total_comments: string;
+};
+
+type PhotoGetParams = {
+  page?: number;
+  total?: number;
+  user?: 0 | string;
+};
+
+export default async function photosGet(
+  { page = 1, total = 6, user = 0 }: PhotoGetParams = {},
+  optionsFront?: RequestInit,
+) {
+  try {
+    const options = optionsFront || {
+      next: {
+        revalidate: 10,
+        tags: ['photos'],
+      },
+    };
+    const { url } = PHOTOS_GET({ page, total, user });
+    const response = await fetch(url, options);
+
+    if (!response.ok) throw new Error('Error downloading images.');
+
+    const data = (await response.json()) as Photo[];
+
+    return { data, ok: true, error: '' };
+  } catch (error) {
+    return apiError(error);
+  }
+}
